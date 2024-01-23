86494d1cbf02c75cf93ec0ba2811bee758a92ea2
thiagoandradedeveloper <thiagoha3@gmail.com>
2024-01-23 01:01:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load",function(){
+    const pos = document.documentElement;
+    pos.addEventListener("mousemove",e => {
+        pos.style.setProperty("--x", e.clientX + "px");
+        pos.style.setProperty("--y", e.clientY + "px");
+    })
+})
